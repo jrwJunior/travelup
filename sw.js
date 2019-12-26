1487a6b1f0f47cbcc63171ae76c79e16efed54a3
Robert <runshacking@gmail.com>
2019-12-27 01:25:14 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-importScripts('/travelup/workbox-v4.3.1/workbox-sw.js');
-
-workbox.setConfig({ modulePathPrefix: '/travelup/workbox-v4.3.1/' });
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 self.addEventListener('message', evt => {
   if (evt.data && evt.data.type === 'SKIP_WAITING') {
@@ -12,12 +10,8 @@ workbox.core.clientsClaim();
 
 const precacheManifest = [
   {
-    "url": "404.html",
-    "revision": "46c90efd5431e54b2607cd2d9a1e7711"
-  },
-  {
     "url": "asset-manifest.json",
-    "revision": "0f28509289b5921dd0f63669813e72df"
+    "revision": "01c7263c608f13df44afae7126546cc2"
   },
   {
     "url": "favicon/apple-touch-icon.png",
@@ -53,11 +47,19 @@ const precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "46c90efd5431e54b2607cd2d9a1e7711"
+    "revision": "76e6f2d4dbe8f7b597e93b31fee30dce"
   },
   {
     "url": "manifest.webmanifest",
     "revision": "5fec3b539377d934e7516a27cfef9d34"
+  },
+  {
+    "url": "precache-manifest.9b1ef2daabd77e9038e2f4b461f32440.js",
+    "revision": "9b1ef2daabd77e9038e2f4b461f32440"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "634fe2e49d9935fed4695f8760dc4838"
   },
   {
     "url": "splash/launch-1125x2436.png",
@@ -88,20 +90,20 @@ const precacheManifest = [
     "revision": "411be3a5350e3379b20fa65e854174e0"
   },
   {
-    "url": "static/css/main.3388b3c7.chunk.css",
-    "revision": "3ff41e237abad766608db3c9e753706f"
+    "url": "static/css/main.c86a47c3.chunk.css",
+    "revision": "d72c8df3535151f221d0feebfcdd4bc9"
   },
   {
-    "url": "static/js/2.6bf4a21d.chunk.js",
-    "revision": "04f8cd8b92b1a25ab3e2cf7416ff4183"
+    "url": "static/js/2.aa9276dd.chunk.js",
+    "revision": "d27d13f4b056749696cc52c52d67df42"
   },
   {
-    "url": "static/js/main.2b540cf7.chunk.js",
-    "revision": "3c3925240c62ab26dddd5966129e464a"
+    "url": "static/js/main.b151a673.chunk.js",
+    "revision": "ca03f323c28767c33b2758de30b00744"
   },
   {
-    "url": "static/js/runtime-main.04d1ea81.js",
-    "revision": "fa82138a23da37fdd21d96bb89ea0076"
+    "url": "static/js/runtime-main.fa0e6e2f.js",
+    "revision": "b72d310a72cf8c35e188a0ece704d95a"
   },
   {
     "url": "static/media/CandaraBold.083a0630.woff2",
@@ -170,127 +172,21 @@ const precacheManifest = [
   {
     "url": "static/media/upload-ios.e4e3f98d.svg",
     "revision": "e4e3f98d4b07591a56a6fd6bfd107ad0"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-background-sync.dev.js",
-    "revision": "5446355eef3aa184b5b6eebfcd81f8d9"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-background-sync.prod.js",
-    "revision": "1ffcc362312a9e8ef4e28280ace2a1bd"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-broadcast-update.dev.js",
-    "revision": "0508d13784c9b0549663f40e3fe55d37"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-broadcast-update.prod.js",
-    "revision": "ee27c0fdc836f6a2dc656b25a680f9e4"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-cacheable-response.dev.js",
-    "revision": "ecba3679d285394f1c9e219681662721"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-cacheable-response.prod.js",
-    "revision": "a38e8afa80070ec9dff5dc2fb116f1c2"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-core.dev.js",
-    "revision": "2912182ccc99b017a8c36802cf9d983f"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-core.prod.js",
-    "revision": "5d14d8267f65030735589e4b664ee3bf"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-expiration.dev.js",
-    "revision": "43c236fe62480f042c35e8b898ca3367"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-expiration.prod.js",
-    "revision": "a767f3bbd2773a0bea34ff841b51ab64"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-navigation-preload.dev.js",
-    "revision": "a8f30e409f7037906053acec7d709beb"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-navigation-preload.prod.js",
-    "revision": "e2b19a3eda50f48ce7fc48640a523353"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-offline-ga.dev.js",
-    "revision": "3fba0947d12d42834b81499fafc76e82"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-offline-ga.prod.js",
-    "revision": "6af4fb51a5249c4e0ed7bc61ed59836d"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-precaching.dev.js",
-    "revision": "8fbbefcd70c998a3cd35f02e6a8ac4e7"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-precaching.prod.js",
-    "revision": "e8f5c57430ec7c448d30015ff4bd5896"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-range-requests.dev.js",
-    "revision": "0f15c57cf5c75cc72b6f23ad28a941d1"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-range-requests.prod.js",
-    "revision": "97c430406d13f4b91c805594ef351261"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-routing.dev.js",
-    "revision": "471b8e0f45e6e5e679d04f60c6466e7f"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-routing.prod.js",
-    "revision": "d3fa76a1c38649d596b1d2ffaf398128"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-strategies.dev.js",
-    "revision": "d1c19737e82e2f6bd567aaf384683174"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-strategies.prod.js",
-    "revision": "6033181992f0bc562ab1ef5f9ba34697"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-streams.dev.js",
-    "revision": "eed9eb6f7b0672c45db5408d05efe9b9"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-streams.prod.js",
-    "revision": "4407a13523f1fa1064f616e9047b6148"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-sw.js",
-    "revision": "6e1e47d706556eac8524f396e785d4bb"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-window.dev.umd.js",
-    "revision": "c17834573a1b48bb8cf33b12128bdae9"
-  },
-  {
-    "url": "workbox-v4.3.1/workbox-window.prod.umd.js",
-    "revision": "c65238721ed1187cf832e51a9e34724a"
   }
 ];
 workbox.precaching.precacheAndRoute(precacheManifest);
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
 
 workbox.routing.registerRoute(
   /(http[s]?:\/\/)restcountries.eu/,
   new workbox.strategies.CacheFirst({
     cacheName: 'api-cache',
   }), 'GET');
+
+  workbox.routing.registerRoute(
+    /.*.(?:png|jpg|jpeg|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'meme-images'
+    }), 'GET');
 
 workbox.routing.registerRoute(
   /(http[s]?:\/\/)firebasestorage.googleapis.com/,
