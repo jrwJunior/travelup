395286bd49a08dc4d358bcf9aae4a1a4834e9a1b
robert <runshacking@gmail.com>
2019-12-29 13:51:55 +0200

    Updates

--- sw.js
+++ sw.js
@@ -51,15 +51,7 @@ const precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "237f935909eee45d02c15796141a9dde"
-  },
-  {
-    "url": "precache-manifest.a5a26e4605cadee3113e8e2664cc7044.js",
-    "revision": "a5a26e4605cadee3113e8e2664cc7044"
-  },
-  {
-    "url": "service-worker.js",
-    "revision": "358e621412f8efa197ba0e5a44137de3"
+    "revision": "e0a8e4fde9a30aeb1b3a9369a878794e"
   },
   {
     "url": "splash/launch-1125x2436.png",
